aeb461c1ec482ec34071f118a6f3f55409e0fcff
Rachel Wu <ruoxuan.wu@mail.utoronto.ca>
2021-06-26 00:27:30 -0400

    adjusted the spacing between the input box and the canvasd

--- git_learning/src/component/canvas.js
+++ git_learning/src/component/canvas.js
@@ -3,14 +3,15 @@ import React from 'react'
 export default function canvas() {
     
     return (
-        <>  
-            <div className="d-flex w-75 justify-content-center align-items-center" style={{maxWidth:"600px"}}>
+        <>  {/* Use the parent div to make the two elements the same width and have spacing at the same time. 
+        As align items center in the parent div doesn't allow it to have spacings*/}
+            <div className="d-flex w-100 justify-content-center align-items-center mb-5" style={{border:"solid yellow"}}>
                 <form>
                     <input type="text"/>
                 </form>
             </div>
             
-                
+            
             <canvas id ="canvas" style={{border:"solid black", width:"100%",height:"100%"}}></canvas>
         </>
         
