141e199a3eff932dbcc84642abfaa37de775aa5e
Rachel Wu <ruoxuan.wu@mail.utoronto.ca>
2021-06-26 23:49:58 -0400

    adjusted spacing between input box and text box

--- git_learning/src/component/canvas.js
+++ git_learning/src/component/canvas.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
 export default function canvas() {
-    
+    function handleSubmit(e){
+        var canvas = document.getElementById("canvas");
+        var ctx= canvas.getContext("2d");
+        
+    }
     return (
-        <>  {/* Use the parent div to make the two elements the same width and have spacing at the same time. 
+        <>  {/* Note: Use the parent div to make the two elements the same width and have spacing at the same time. 
         As align items center in the parent div doesn't allow it to have spacings*/}
-            <div className="d-flex w-100 justify-content-center align-items-center mb-5" style={{border:"solid yellow"}}>
-                <form>
-                    <input type="text"/>
-                </form>
-            </div>
-            
+            <form onSubmit={handleSubmit}>
+                <div className="d-flex flex-column w-100 justify-content-center align-items-center mb-5" style={{border:"solid yellow"}}>
+                    <input type="text" className="m-3"/>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </form>
             
             <canvas id ="canvas" style={{border:"solid black", width:"100%",height:"100%"}}></canvas>
         </>
