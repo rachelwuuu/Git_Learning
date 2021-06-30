025c016c86a42f388e372b51a3f599d6e57863f2
Rachel Wu <ruoxuan.wu@mail.utoronto.ca>
2021-06-29 23:58:56 -0400

    drew a vertical arrow

--- git_learning/src/App.js
+++ git_learning/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import canvas from './component/canvas'
-
+{/*1.reactjs 2. bootstrap 3.svg from w3c 4. router?*/}
 export default function App() {
     return (
         <Container className="d-flex justify-content-center align-items-center" style={{
--- git_learning/src/component/canvas.js
+++ git_learning/src/component/canvas.js
@@ -33,6 +33,14 @@ export default function canvas() {
             <div id="parentDiv" className="d-flex flex-column w-100" style={{border:"solid orange"}}>
 
             </div>
+            <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <marker id="arrowHead" viewBox="0 0 10 10" markerWidth="6" markerHeight="6" refX="5" refY="5" orient="auto-start-reverse">
+                        <path d="M 0 0 L 10 5 L 0 10 z"></path>
+                    </marker>
+                </defs>
+                <polyline points="10, 10 10, 90" fill="none" stroke="black" markerStart="url(#arrowHead)" markerEnd="url(#arrowHead)"></polyline>
+            </svg>
            {/* <canvas id ="canvas" style={{border:"solid black", width:"100%",height:"100%"}}></canvas>*/}
         </>
         
