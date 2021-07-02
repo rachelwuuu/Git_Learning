41329f28dde0732da192440811032336ce296313
Rachel Wu <ruoxuan.wu@mail.utoronto.ca>
2021-07-02 02:32:46 -0400

    added a vertical line

--- git_learning/src/component/canvas.js
+++ git_learning/src/component/canvas.js
@@ -9,15 +9,43 @@ export default function canvas() {
         
         var commitInfo=document.getElementById("commit_information").value
         var newCommitArray=commitInfo.split(/(?=commit)/g);
-        newCommitArray.forEach(element => {
+        newCommitArray.reverse().forEach(element => {
             var newCommit=document.createElement('div')
             var parentDiv=document.getElementById("parentDiv")
-            if(divCreationCount!=0||divCreationCount!=newCommitArray.length-2){
+            console.log("div"+newCommitArray.length)
+            var lastElementId=newCommitArray-1;
+            if(divCreationCount>0||divCreationCount<(lastElementId)){
+                console.log("...")
+                var path=document.createElementNS('http://www.w3.org/2000/svg',"path")
+                path.setAttributeNS(null,"d", "M 0 0 L 10 5 L 0 10 z")
+                var marker=document.createElementNS('http://www.w3.org/2000/svg',"marker")
+                marker.setAttribute("id","arrowHead")
+                marker.setAttribute("viewBox", "0 0 10 10")
+                marker.setAttribute("markerWidth", "6")
+                marker.setAttribute("markerHeight", "6")
+                marker.setAttribute("refX", "5")
+                marker.setAttribute("refY", "5")
+                marker.setAttribute("orient", "auto-start-reverse")
+                var polyline=document.createElementNS('http://www.w3.org/2000/svg',"polyline")
+                polyline.setAttribute("points","10, 10 10, 90")
+                polyline.setAttribute("fill","none")
+                polyline.setAttribute("stroke","black")
+                polyline.setAttribute("markerEnd","url(#arrowHead)")
+                var defs=document.createElementNS('http://www.w3.org/2000/svg','defs')
                 var arrow=document.createElementNS('http://www.w3.org/2000/svg',"svg")
+                arrow.setAttribute("viewBox","0 0 100 100")
+                arrow.setAttributeNS('http://www.w3.org/2000/xmlns/',"xmlns:xlink",'http://www.w3.org/2000/xmlns/')
                 var connectingDiv=document.createElement('div')
                 connectingDiv.style.border= "solid red";
-                connectingDiv.id="newCommit"+divCreationCount-1;
+                connectingDiv.id="connectingDiv"+(divCreationCount);
+                
                 parentDiv.appendChild(connectingDiv)
+                connectingDiv.appendChild(arrow)
+                arrow.appendChild(defs)
+                defs.appendChild(marker)
+                marker.appendChild(path)
+                arrow.appendChild(polyline)
+               
             }
             
             divCreationCount+=1;
@@ -26,8 +54,6 @@ export default function canvas() {
             newCommit.innerHTML=element
             
             parentDiv.appendChild(newCommit)
-            var newCommitOffset=newCommit.offsetHeight
-            console.log(newCommitOffset+";")
         });
         
         
