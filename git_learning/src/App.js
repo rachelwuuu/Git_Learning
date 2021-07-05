619671c217a7b208f222ead1aee4a8024d97e826
Rachel Wu <ruoxuan.wu@mail.utoronto.ca>
2021-07-05 00:24:13 -0400

    added error messgaes

--- git_learning/src/App.js
+++ git_learning/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import canvas from './component/canvas'
-{/*1.reactjs 2. bootstrap 3.svg from w3c 4. router? 5.JQuery*/}
+{/*1.reactjs 2. bootstrap 3.svg from w3c 4. router? 5.react-bootstrap 6.regex */}
 export default function App() {
     return (
         <Container className="d-flex justify-content-center align-items-center" style={{
--- git_learning/src/component/canvas.js
+++ git_learning/src/component/canvas.js
@@ -1,65 +1,86 @@
 import React from 'react'
-
+import {Card} from "react-bootstrap"
 export default function canvas() {
     let divCreationCount=0;
     let commitBoxIdArray=[]
     function handleSubmit(e){
         e.preventDefault()
+        ///////////1.Separate the text from input box and store them in an array
         var commitInfo=document.getElementById("commit_information").value
-        var newCommitArray=commitInfo.split(/(?=commit)/g);
-        newCommitArray.reverse().forEach(element => {
-            var newCommit=document.createElement('div')
-            var parentDiv=document.getElementById("parentDiv")
-            var lastElementId=newCommitArray-1;
-            if(divCreationCount>0||divCreationCount<(lastElementId)){
-                var path=document.createElementNS('http://www.w3.org/2000/svg',"path")
-                path.setAttributeNS(null,"d", "M 0 0 L 10 5 L 0 10 z")
-                var marker=document.createElementNS('http://www.w3.org/2000/svg',"marker")
-                marker.setAttribute("id","arrowHead")
-                marker.setAttribute("viewBox", "0 0 10 10")
-                marker.setAttribute("markerWidth", "3")
-                marker.setAttribute("markerHeight", "3")
-                marker.setAttribute("refX", "5")
-                marker.setAttribute("refY", "5")
-                marker.setAttribute("orient", "auto-start-reverse")
-                var polyline=document.createElementNS('http://www.w3.org/2000/svg',"polyline")
-                polyline.setAttribute("points","50, 0 50, 20")
-                polyline.setAttribute("fill","none")
-                polyline.setAttribute("stroke","black")
-                polyline.setAttribute("marker-end","url(#arrowHead)")
-                var defs=document.createElementNS('http://www.w3.org/2000/svg','defs')
-                var arrow=document.createElementNS('http://www.w3.org/2000/svg',"svg")
-                arrow.setAttribute("viewBox","0 0 100 22")
-                arrow.setAttributeNS('http://www.w3.org/2000/xmlns/',"xmlns:xlink",'http://www.w3.org/2000/xmlns/')
-                var connectingDiv=document.createElement('div')
-                connectingDiv.style.background= "solid red";
-                connectingDiv.id="connectingDiv"+(divCreationCount);
-                
-                parentDiv.appendChild(connectingDiv)
-                connectingDiv.appendChild(arrow)
-                arrow.appendChild(defs)
-                defs.appendChild(marker)
-                marker.appendChild(path)
-                arrow.appendChild(polyline)
-               
-            }
-            
-            divCreationCount+=1;
-            newCommit.style.border= "solid grey";
-            newCommit.id="newCommit"+divCreationCount;
-            commitBoxIdArray.push(newCommit.id)
-            newCommit.innerHTML=element
-            newCommit.addEventListener("click",function(e)
-            {
-                for(var i=0; i<commitBoxIdArray.length;i++){
-                    if(document.getElementById(commitBoxIdArray[i]).style.border==="solid green"){
-                        document.getElementById(commitBoxIdArray[i]).style.border="solid grey";
+        //commit d8403b71c9f44d6e619651dbf51e24ec66a487e7
+        if (commitInfo.startsWith("commit ")){
+            var newCommitArray=commitInfo.split(/(?=commit)/g);
+            //////////////2. Loop through the array and draw the elements dynamically on the website
+            newCommitArray.reverse().forEach(element => {
+                var commitId=element.substring(7,47)
+                var patternOfGitCommitId=/[\w]{40}/
+                console.log(element.charAt(47))
+                if(patternOfGitCommitId.test(commitId) && element.charAt(47)===" "){
+                    var newCommit=document.createElement('div')
+                    var parentDiv=document.getElementById("parentDiv")
+                    var lastElementId=newCommitArray-1;
+                    ////////(1)draw svg of the arrow between the commit boxes within the if statement//////////
+                    if(divCreationCount>0||divCreationCount<(lastElementId)){
+                        var path=document.createElementNS('http://www.w3.org/2000/svg',"path")
+                        path.setAttributeNS(null,"d", "M 0 0 L 10 5 L 0 10 z")
+                        var marker=document.createElementNS('http://www.w3.org/2000/svg',"marker")
+                        marker.setAttribute("id","arrowHead")
+                        marker.setAttribute("viewBox", "0 0 10 10")
+                        marker.setAttribute("markerWidth", "3")
+                        marker.setAttribute("markerHeight", "3")
+                        marker.setAttribute("refX", "5")
+                        marker.setAttribute("refY", "5")
+                        marker.setAttribute("orient", "auto-start-reverse")
+                        var polyline=document.createElementNS('http://www.w3.org/2000/svg',"polyline")
+                        polyline.setAttribute("points","50, 0 50, 20")
+                        polyline.setAttribute("fill","none")
+                        polyline.setAttribute("stroke","black")
+                        polyline.setAttribute("marker-end","url(#arrowHead)")
+                        var defs=document.createElementNS('http://www.w3.org/2000/svg','defs')
+                        var arrow=document.createElementNS('http://www.w3.org/2000/svg',"svg")
+                        arrow.setAttribute("viewBox","0 0 100 22")
+                        arrow.setAttributeNS('http://www.w3.org/2000/xmlns/',"xmlns:xlink",'http://www.w3.org/2000/xmlns/')
+                        var connectingDiv=document.createElement('div')
+                        connectingDiv.id="connectingDiv"+(divCreationCount);
+                        
+                        parentDiv.appendChild(connectingDiv)
+                        connectingDiv.appendChild(arrow)
+                        arrow.appendChild(defs)
+                        defs.appendChild(marker)
+                        marker.appendChild(path)
+                        arrow.appendChild(polyline)
+                    
                     }
+                    
+                    divCreationCount+=1;
+                    newCommit.style.border= "solid grey";
+                    newCommit.id="newCommit"+divCreationCount;
+                    commitBoxIdArray.push(newCommit.id)
+                    newCommit.innerHTML=element
+                    newCommit.addEventListener("click",function(e)
+                    {
+                        for(var i=0; i<commitBoxIdArray.length;i++){
+                            if(document.getElementById(commitBoxIdArray[i]).style.border==="solid green"){
+                                document.getElementById(commitBoxIdArray[i]).style.border="solid grey";
+                            }
+                        }
+                        document.getElementById(newCommit.id).style.border="solid green"
+                    })
+                    parentDiv.appendChild(newCommit)
+                }else{
+                    console.log("Please enter a commit message with a valid commit id.")
                 }
-                document.getElementById(newCommit.id).style.border="solid green"
-            })
-            parentDiv.appendChild(newCommit)
-        });   
+                
+            });   
+            
+            
+        }else if(commitInfo.startsWith("git ")){
+
+        }else{
+            console.log("Please add a commit message or a git command")
+        }
+        
+        
     }
     return (
         <>  {/* Note: Use the parent div to make the two elements the same width and have spacing at the same time. 
@@ -70,9 +91,9 @@ export default function canvas() {
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </div>
             </form>
-            <div id="parentDiv" className="d-flex flex-column w-100" style={{border:"solid orange"}}>
+            <Card id="parentDiv" className="d-flex flex-column w-100">
 
-            </div>
+            </Card>
            
         </>
         
